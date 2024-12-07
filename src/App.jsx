a3b6555375c797683bd59378f828a67a251f46ee
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-12-07 23:27:05 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import clsx from "clsx"
+import { useState } from "react"
+import { languages } from "./languages"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function AssemblyEndgame() {
+  // eslint-disable-next-line no-unused-vars
+  const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  const WrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length
+
+  const hasWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
+  const hasLost = WrongGuessCount >= languages.length
+
+  const isGameOver = hasLost || hasWon
+
+  // function getRandomWord() {
+  //   const randomNumber = Math.floor(Math.random() * words.length)
+  //   return words[randomNumber]
+  // }
+
+  function handleGuessLetter(letter) {
+    setGuessedLetters(prev => [...prev, letter])
+  }
+
+
+  const languageElements = languages.map((lang, index) => {
+    const lost = index < WrongGuessCount
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+    return (
+      <span
+        className={`chip ${lost ? "lost" : ""}`}
+        style={styles}
+        key={lang.name}
+      >
+        {lang.name}
+      </span>
+    )
+  })
+
+  const letterElements = currentWord.split("").map((letter, index) => {
+    const show = guessedLetters.includes(letter)
+    return (
+      <span key={index}>
+        {show && (
+          <span>
+            {letter.toUpperCase()}
+          </span>
+        )}
+      </span>
+    )
+  })
+
+  const keyboardElements = alphabet.split("").map(letter => {
+    const isGuessed = guessedLetters.includes(letter)
+    const isCorrect = isGuessed && currentWord.includes(letter)
+    const style = {
+      backgroundColor: isCorrect ? "green" : isGuessed && "red"
+    }
+    return (
+      <button
+        key={letter}
+        onClick={() => handleGuessLetter(letter)}
+        disabled={isGuessed}
+        style={style}
+      >
+        {letter.toUpperCase()}
+      </button>
+    )
+  })
+
+
+  const gameStatusClass = clsx("game-status", {
+    won: hasWon,
+    lost: hasLost
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the
+          programming world safe from Assembly!</p>
+      </header>
+      <section className={gameStatusClass}>
+        {hasWon && (
+          <>
+            <h2>You win!</h2>
+            <p>Well done! 🎉</p>
+          </>
+        )}
+        {hasLost && (
+          <>
+            <h2>Game over!</h2>
+            <p>You lose! Better start learning Assembly 😭</p>
+          </>
+        )}
+
+      </section>
+      <section className="language-chips">
+        {languageElements}
+      </section>
+      <section className="word">
+        {letterElements}
+      </section>
+      <section className="keyboard">
+        {keyboardElements}
+      </section>
+      {
+        isGameOver &&
+        <button className="new-game">New Game</button>
+      }
+    </main>
   )
 }
-
-export default App
